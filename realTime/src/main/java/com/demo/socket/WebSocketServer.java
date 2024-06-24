83bbe3d40ed970c5540b061ea62955ab77eae271
KimJaeWoo <KimJaeWoo@DESKTOP-6RODQMA>
2024-06-24 18:14:06 +0900

    웹 소켓 처리 추가

--- realTime/src/main/java/com/demo/socket/WebSocketServer.java
+++ realTime/src/main/java/com/demo/socket/WebSocketServer.java
@@ -12,6 +12,15 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+/* @ServerEndpoint
+ * 웹 소켓 연결을 처리하는 클래스
+ * 웹 소켓 연결 수명 주기 메서드를 구현
+ * 메서드들은 웹 소켓 연결의 생성, 메시지 수신, 연결 종료 등의 이벤트를 처리
+ * 웹 소켓 세션 객체를 통해 클라이언트와의 통신을 처리
+ * 
+ * 어노테이션 없이 Endpoint 인터페이스의 추상 메서드 구현 가능 extends Endpoint
+ * 단점 : 애노테이션 방식에 비해 구현이 다소 복잡하고 엔드포인트 설정이 프로그래밍 방식으로 이루어져야 한다는 단점
+ * */
 @ServerEndpoint("/websocket")
 public class WebSocketServer{
 	private static Set<Session> clients = 
